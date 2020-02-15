f9d575bd0fa30a923b6f825c5ffed2c2c0a14159
Emadmagdy151995Fci <emadmagdi.151995@gmai.com>
2020-02-16 01:37:35 +0200

    emad

--- app/src/main/java/com/taibah/fm_app/activities_fragments/activity_health_food/HealthFoodActivity.java
+++ app/src/main/java/com/taibah/fm_app/activities_fragments/activity_health_food/HealthFoodActivity.java
@@ -1,16 +1,63 @@
 package com.taibah.fm_app.activities_fragments.activity_health_food;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+
 import com.taibah.fm_app.R;
+import com.taibah.fm_app.databinding.ActivityHealthFoodBinding;
+import com.taibah.fm_app.interfaces.Listeners;
+import com.taibah.fm_app.language.LanguageHelper;
 
-public class HealthFoodActivity extends AppCompatActivity {
+import java.util.Locale;
 
+import io.paperdb.Paper;
+
+public class HealthFoodActivity extends AppCompatActivity implements Listeners.BackListener{
+    private ActivityHealthFoodBinding binding;
+    private String lang;
+
+
+
+
+
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        Paper.init(newBase);
+        super.attachBaseContext(LanguageHelper.updateResources(newBase, Paper.book().read("lang","ar")));
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_health_food);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_health_food);
+        initView();
     }
+
+
+
+
+    private void initView()
+    {
+        Paper.init(this);
+        lang = Paper.book().read("lang", Locale.getDefault().getLanguage());
+        binding.setBackListener(this);
+        binding.setLang(lang);
+        binding.progBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(this,R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+
+
+
+    }
+
+
+
+    @Override
+    public void back() {
+        finish();
+    }
+
 }
